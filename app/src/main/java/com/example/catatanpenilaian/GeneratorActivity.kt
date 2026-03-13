477a17e8ee92931ad7a2c83b175a8eb0db8ed9a0
wsantika <wahyuputra7396@gmail.com>
2026-03-13 22:59:40 +0800

    Improve UI design with modern card layout, fix crash issue, and enhance login & generator screens

--- app/src/main/java/com/example/catatanpenilaian/GeneratorActivity.kt
+++ app/src/main/java/com/example/catatanpenilaian/GeneratorActivity.kt
@@ -12,18 +12,25 @@ import android.widget.Button
 class GeneratorActivity : AppCompatActivity() {
     lateinit var tvGreeting: TextView
     lateinit var etJumlah: EditText
-    lateinit var etNilai: EditText
     lateinit var btnProses: Button
     lateinit var tvHasil: TextView
+    lateinit var etTugas: EditText
+    lateinit var etKeaktifan: EditText
+    lateinit var etUTS: EditText
+    lateinit var etUAS: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_generator)
         tvGreeting = findViewById(R.id.tvGreeting)
         etJumlah = findViewById(R.id.etJumlah)
-        etNilai = findViewById(R.id.etNilai)
         btnProses = findViewById(R.id.btnProses)
         tvHasil = findViewById(R.id.tvHasil)
+        etTugas = findViewById(R.id.etTugas)
+        etKeaktifan = findViewById(R.id.etKeaktifan)
+        etUTS = findViewById(R.id.etUTS)
+        etUAS = findViewById(R.id.etUAS)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -33,14 +40,20 @@ class GeneratorActivity : AppCompatActivity() {
         tvGreeting.text = "Selamat bertugas, Dosen $namaDosen"
         btnProses.setOnClickListener {
 
-            val jumlah = etJumlah.text.toString().toInt()
-            val nilai = etNilai.text.toString().toDouble()
+            val jumlah = etJumlah.text.toString().toIntOrNull() ?: 0
+
+            val tugas = etTugas.text.toString().toDoubleOrNull() ?: 0.0
+            val keaktifan = etKeaktifan.text.toString().toDoubleOrNull() ?: 0.0
+            val uts = etUTS.text.toString().toDoubleOrNull() ?: 0.0
+            val uas = etUAS.text.toString().toDoubleOrNull() ?: 0.0
+
+            val nilaiAkhir = (tugas + keaktifan + uts + uas) / 4
 
             var status = ""
 
-            if (nilai >= 80) {
+            if (nilaiAkhir >= 80) {
                 status = "Sangat Baik"
-            } else if (nilai >= 60) {
+            } else if (nilaiAkhir >= 60) {
                 status = "Cukup"
             } else {
                 status = "Kurang"
@@ -52,7 +65,10 @@ class GeneratorActivity : AppCompatActivity() {
                 daftar += "Mahasiswa $i : ______\n"
             }
 
-            tvHasil.text = "Status kelas: $status\n\nDaftar Absen:\n$daftar"
+            tvHasil.text =
+                "Nilai rata-rata kelas: $nilaiAkhir\n" +
+                        "Status kelas: $status\n\n" +
+                        "Daftar Absen:\n$daftar"
         }
     }
 }
